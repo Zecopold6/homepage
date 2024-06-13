51f358db2e037ef7a78e289affc4b0c45e33add3
Zecopold6 <135575594+Zecopold6@users.noreply.github.com>
2024-06-14 03:33:14 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -208,7 +208,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     
     
-    //pop('./static/img/tz.jpg')
+    //pop('')
     
     
     
@@ -217,7 +217,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-var pageLoading = document.querySelector("#zyyo-loading");
+var pageLoading = document.querySelector("#zc-loading");
 window.addEventListener('load', function() {
     setTimeout(function () {
         pageLoading.style.opacity = '0';
