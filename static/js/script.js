a7c0a62c76feb4a0c02cf0835c3a718a193d1ad9
Zecopold6 <135575594+Zecopold6@users.noreply.github.com>
2024-06-14 03:39:30 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,4 @@
-console.log('%cCopyright © 2024 ZCOS',
-    'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;'
-);
-console.log('%c   z', 'color: #8B4513; font-size: 20px;');
-console.log('%c  c', 'color: #8B4513; font-size: 20px;');
-console.log(' %c  o', 'color: #8B4513; font-size: 20px;');
-console.log('  %c s', 'color: #8B4513; font-size: 20px;');
-console.log('  %c2024', 'color: #8B4513; font-size: 20px;');
+
 
 document.addEventListener('contextmenu', function (event) {
     event.preventDefault();
